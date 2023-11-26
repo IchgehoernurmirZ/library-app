1fc20509994e36590a526550dced949ae3ec77a7
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-11-26 11:53:19 -0500

    add routing for homepage

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
+import { Route } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
       <Navbar/>
-      <HomePage/>
+      <Route path='/'>
+        <HomePage/>
+      </Route>
       <Footer/>
     </div>
   );
