e579b0540c10e53296b44ebe829af93ff4a11218
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-11-26 12:02:04 -0500

    improve routing for homepage

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -3,16 +3,21 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
-import { Route } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Route path='/'>
-        <HomePage/>
-      </Route>
-      <Footer/>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to='/home'/>
+        </Route>
+        <Route path='/home'>
+          <HomePage/>
+        </Route>
+      </Switch>
+      <Footer />
     </div>
   );
 }
