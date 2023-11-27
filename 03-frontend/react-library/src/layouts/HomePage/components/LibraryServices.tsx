9b3718de22e3eb4bfb154926a90fa600a54a037a
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-11-27 01:47:38 -0500

    hide sign up buttons when logged in

--- 03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
@@ -1,4 +1,8 @@
+import { Link } from "react-router-dom"
+import { useOktaAuth } from "@okta/okta-react"
+
 export const LibraryServices = () => {
+    const { authState } = useOktaAuth();
     return (
         <div className="container my-5">
             <div className="row p-4 align-items-center border shadow-lg">
@@ -11,7 +15,23 @@ export const LibraryServices = () => {
                         send our library admin a personal message!
                     </p>
                     <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
-                        <a href="#" className="btn main-color btn-lg text-white">Sign up</a>
+                    {authState?.isAuthenticated ? (
+                <Link
+                  type="button"
+                  className="btn main-color btn-lg px-4 me-md-2 fw-bold text-white"
+                  to="#"
+                >
+                  Library Services
+                </Link>
+              ) : (
+                <Link
+                  type="button"
+                  className="btn main-color btn-lg text-white"
+                  to="/login"
+                >
+                  Log In
+                </Link>
+              )}
                     </div>
                 </div>
                 <div className="col-lg-4 offset-lg-1 shadow-lg lost-image"></div>
