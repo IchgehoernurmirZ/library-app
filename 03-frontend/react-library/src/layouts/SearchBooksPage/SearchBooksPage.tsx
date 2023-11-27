a0afa34c2dd681baac1de4303cd8346095bf8543
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-11-27 13:51:47 -0500

    improve search book page

--- 03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ 03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -2,16 +2,25 @@ import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { SearchBook } from "./components/SearchBook";
+import { Pagination } from "../Utils/Pagination";
 
 export const SearchBooksPage = () => {
     const [books, setBooks] = useState<BookModel[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const [booksPerPage] = useState(5);
+    const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [search, setSearch] = useState('');
+    const [searchUrl, setSearchUrl] = useState('');
+    const [categorySelection, setCategorySelection] = useState('Book category');
+
     useEffect(() => {
         const fetchBooks = async () => {
           const baseUrl: string = "http://localhost:8080/api/books";
-          const url: string = `${baseUrl}?page=0&size=5`;
+          const url: string = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
     
           const response = await fetch(url);
     
@@ -22,6 +31,9 @@ export const SearchBooksPage = () => {
           const responseJson = await response.json();
     
           const responseData = responseJson._embedded.books;
+
+          setTotalAmountOfBooks(responseJson.page.totalElements);
+          setTotalPages(responseJson.page.totalPages);
     
           const loadedBooks: BookModel[] = [];
     
@@ -46,7 +58,8 @@ export const SearchBooksPage = () => {
           setIsLoading(false);
           setHttpError(error.message);
         })
-      }, []);
+        window.scrollTo(0, 0);
+      }, [currentPage]);
 
       if (isLoading) {
         return (
@@ -61,6 +74,13 @@ export const SearchBooksPage = () => {
           </div>
         )
       }
+
+      const indexOfLastBook: number = currentPage * booksPerPage;
+      const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+      let lastItem = booksPerPage * currentPage <= totalAmountOfBooks ?
+          booksPerPage * currentPage : totalAmountOfBooks;
+  
+      const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
       return (
         <div>
@@ -89,31 +109,71 @@ export const SearchBooksPage = () => {
                     >
                       Category
                     </button>
-                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li>
-                            <a className="dropdown-item" href="#">All</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Front End</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Back End</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Data</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">DevOps</a>
-                        </li>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenuButton1"
+                    >
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          All
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Front End
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Back End
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Data
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          DevOps
+                        </a>
+                      </li>
                     </ul>
                   </div>
                 </div>
               </div>
-              <div className="mt-3">
-                <h5>Number of results: (22)</h5>
-              </div>
-              <p>1 to 5 of 22 items:</p>
-              {books.map(book => (<SearchBook book={book} key={book.id}/>))}
+              {totalAmountOfBooks > 0 ? (
+                <>
+                  <div className="mt-3">
+                    <h5>Number of results: ({totalAmountOfBooks})</h5>
+                  </div>
+                  <p>
+                    {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks}{" "}
+                    items:
+                  </p>
+                  {books.map((book) => (
+                    <SearchBook book={book} key={book.id} />
+                  ))}
+                </>
+              ) : (
+                <div className="m-5">
+                  <h3>Can't find what you are looking for?</h3>
+                  <a
+                    type="button"
+                    className="btn main-color btn-md px-4 me-md-2 fw-bold text-white"
+                    href="#"
+                  >
+                    Library Services
+                  </a>
+                </div>
+              )}
+              {totalPages > 1 && (
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  paginate={paginate}
+                />
+              )}
             </div>
           </div>
         </div>
