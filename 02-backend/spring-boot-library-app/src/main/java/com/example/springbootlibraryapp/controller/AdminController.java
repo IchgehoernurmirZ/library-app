177d965c8b909620ba55d61c47e3e360d816c331
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-11-27 11:41:50 -0500

    add book backend

---
+++ 02-backend/spring-boot-library-app/src/main/java/com/example/springbootlibraryapp/controller/AdminController.java
@@ -0,0 +1,24 @@
+package com.example.springbootlibraryapp.controller;
+
+import com.example.springbootlibraryapp.requestmodels.AddBookRequest;
+import com.example.springbootlibraryapp.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/add/book")
+    public void postBook(@RequestBody AddBookRequest addBookRequest) throws Exception {
+        adminService.postBook(addBookRequest);
+    }
+}
