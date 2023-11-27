1a40965cd46ac99d6ef447164ff79aaa31747804
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-11-27 12:10:24 -0500

    create add new book component

--- 03-frontend/react-library/src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ 03-frontend/react-library/src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -1,6 +1,7 @@
 import { useOktaAuth } from "@okta/okta-react"
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
+import { AddNewBook } from "./AddNewBook";
 
 export const ManageLibraryPage = () => {
     const { authState } = useOktaAuth();
@@ -44,7 +45,7 @@ export const ManageLibraryPage = () => {
                 aria-controls="nav-add-book"
                 aria-selected="false"
               >
-                Add new book
+                <AddNewBook/>
               </button>
               <button
                 onClick={changeQtyOfBooksClickFunction}
