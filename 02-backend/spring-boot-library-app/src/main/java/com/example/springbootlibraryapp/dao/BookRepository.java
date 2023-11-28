38ff8b4cd22b01576c8243bf57ad0dfd36a65756
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-11-28 00:32:56 -0500

    change routing

--- 02-backend/spring-boot-library-app/src/main/java/com/example/springbootlibraryapp/dao/BookRepository.java
+++ 02-backend/spring-boot-library-app/src/main/java/com/example/springbootlibraryapp/dao/BookRepository.java
@@ -2,8 +2,11 @@ package com.example.springbootlibraryapp.dao;
 
 import com.example.springbootlibraryapp.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
 
 }
--- 03-frontend/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
+++ 03-frontend/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
@@ -17,7 +17,7 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel, deleteBook: any }
     }, []);
 
     async function increaseQuantity() {
-        const url = `http://localhost:8080/api/admin/increase/book/quantity/?bookId=${props.book?.id}`;
+        const url = `http://localhost:8080/api/admin/increase/book/quantity?bookId=${props.book?.id}`;
         const requestOptions = {
             method: 'PUT',
             headers: {
@@ -34,7 +34,7 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel, deleteBook: any }
     }
 
     async function decreaseQuantity() {
-        const url = `http://localhost:8080/api/admin/decrease/book/quantity/?bookId=${props.book?.id}`;
+        const url = `http://localhost:8080/api/admin/decrease/book/quantity?bookId=${props.book?.id}`;
         const requestOptions = {
             method: 'PUT',
             headers: {
@@ -51,7 +51,7 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel, deleteBook: any }
     }
 
     async function deleteBook() {
-        const url = `http://localhost:8080/api/admin/delete/book/?bookId=${props.book?.id}`;
+        const url = `http://localhost:8080/api/admin/delete/book?bookId=${props.book?.id}`;
         const requestOptions = {
             method: 'DELETE',
             headers: {
